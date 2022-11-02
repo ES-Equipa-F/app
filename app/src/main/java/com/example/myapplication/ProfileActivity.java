ca2f72a8e1c969dc6f328cd620bf5af82cd286b3
helena-goncalves <78916982+helena-goncalves@users.noreply.github.com>
2022-11-02 10:34:42 +0000

    Add files via upload

--- app/src/main/java/com/example/myapplication/ProfileActivity.java
+++ app/src/main/java/com/example/myapplication/ProfileActivity.java
@@ -1,22 +1,40 @@
 package com.example.myapplication;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Objects;
+
+
 public class ProfileActivity extends AppCompatActivity {
 
+    private EditText oldPass, newPass;
+    private Button buttonChange;
+    //private String oldPassstr, newPassstr; //tem a pass que o user vai inserir
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        newPass = (EditText) findViewById(R.id.editNewPassword);
+        oldPass = (EditText) findViewById(R.id.editOldPassword);
+        buttonChange = findViewById(R.id.change_but);
+
         goToHome();
         goToSmart();
+        ChangePassword(oldPass);
     }
 
     public void goToHome(){
@@ -41,5 +59,33 @@ public class ProfileActivity extends AppCompatActivity {
         });
     }
 
-
+    public void ChangePassword(EditText oldPass) {
+/*
+        String oldPass_str = oldPass.getText().toString();
+        String oldDB="aaaa";
+        buttonChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (oldDB.equals(oldPass_str)) {
+                    Context context1 = getApplicationContext();
+                    Toast toast1 = Toast.makeText(getApplicationContext(), "Password Updated!", Toast.LENGTH_SHORT);
+                    toast1.show();
+                }
+                else{
+                    Context context2 = getApplicationContext();
+                    Toast toast2 = Toast.makeText(getApplicationContext(), "ERROR", Toast.LENGTH_SHORT);
+                    toast2.show();
+                }
+            }
+        });*/
+        buttonChange.setOnClickListener(event -> {
+            if (oldPass.getText().equals("xxxx")) {
+                Toast.makeText(getApplicationContext(), "Password updated!", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "Error!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 }
+
+
