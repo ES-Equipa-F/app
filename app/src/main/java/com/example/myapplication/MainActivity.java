5892d0cb1fa6159f127f7ca6f786c62b9fa0ef6b
João Mina <78810007+J-Mina@users.noreply.github.com>
2022-11-02 09:51:26 +0000

    Login Scheme

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -37,6 +38,33 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent i = new Intent(MainActivity.this,signUpActivity.class);
                 startActivity(i);
+            }
+        });
+    }
+
+    public void LogIn(){
+
+        EditText UserName,Password;
+
+        Button button = (Button) findViewById(R.id.login_but);
+        UserName = findViewById(R.id.editTextTextPersonName);
+        Password = findViewById(R.id.editTextTextPassword);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username, password;
+                username = String.valueOf(UserName.getText());
+                password = String.valueOf(Password.getText());
+
+                /* if(Make query to check if the username and password correspond to something in the db){
+
+                }
+                else{
+                    Make a toast saying "Incorrect username or password, try again! "
+                }
+                * */
+
             }
         });
     }
