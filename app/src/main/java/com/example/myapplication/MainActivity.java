ee7b9b0372797fb079e25608a3e9921932ed8d39
pedrosilva2703 <up201806526@fe.up.pt>
2022-12-01 13:07:21 +0000

    database connection

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,17 +2,35 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String url = "jdbc:mysql://34.163.74.192/mydb?autoReconnect=true&useSSL=false";
+    private static final String user = "root";
+    private static final String pass = "db-es-teamf";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ConnectMySql connectMySql = new ConnectMySql();
+        connectMySql.execute("");
 
         goToMyHome();
         goToSignUp();
@@ -67,6 +85,62 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+
+
+
+    private class ConnectMySql extends AsyncTask<String, Void, String> {
+        String res = "";
+        Connection con = null;
+
+
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                ResultSet rs = null;
+                String s = null;
+                Class.forName("com.mysql.jdbc.Driver");
+                Connection con = DriverManager.getConnection(url, user, pass);
+                Statement st = con.createStatement();
+                rs = st.executeQuery("Select * from house where id = 1");
+
+                try{
+                    if(rs.next()){
+                        s = rs.getString("address");
+
+                    }
+                }
+                catch (Exception e){
+                    System.out.println(e);
+                }
+                System.out.println(s);
+
+
+                String result = "Database Connection Successful\n";
+                res = result;
+            } catch (SQLException e) {
+                e.printStackTrace();
+                res = e.toString();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } finally {
+                if(con != null){
+                    try {
+                        con.close();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return res;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Log.i(this.getClass().getSimpleName(), result);
+            //escrever aqui o que fazer com os resultados
+        }
     }
 
 
