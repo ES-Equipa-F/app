4c678fdbb43a0d4147182203f8f637085388a35d
João Mina <78810007+J-Mina@users.noreply.github.com>
2022-12-05 22:42:43 +0000

    myProfile with DB

--- app/src/main/java/com/example/myapplication/ProfileActivity.java
+++ app/src/main/java/com/example/myapplication/ProfileActivity.java
@@ -3,30 +3,35 @@ package com.example.myapplication;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Objects;
 
 
 public class ProfileActivity extends AppCompatActivity {
 
-    private EditText oldPass, newPass;
-    private Button buttonChange;
-    //private String oldPassstr, newPassstr; //tem a pass que o user vai inserir
+    private static final String url = "jdbc:mysql://34.163.74.192/mydb?autoReconnect=true&useSSL=false";
+    private static final String user = "root";
+    private static final String pass = "db-es-teamf";
 
     String current_email="";
     String current_house_id="";
 
-
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,13 +40,27 @@ public class ProfileActivity extends AppCompatActivity {
         current_email = getIntent().getStringExtra("message_email");
         current_house_id = getIntent().getStringExtra("message_house_id");
 
-        newPass = (EditText) findViewById(R.id.editNewPassword);
-        oldPass = (EditText) findViewById(R.id.editOldPassword);
-        buttonChange = findViewById(R.id.change_but);
+        ProfileActivity.displayNameEmail display = new ProfileActivity.displayNameEmail();
+        display.execute();
 
         goToHome();
         goToSmart();
-        ChangePassword(oldPass);
+        ChangePassword();
+        Logout();
+    }
+
+    public void Logout(){
+        Button buttonLogOut = findViewById(R.id.logout);
+
+        buttonLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(ProfileActivity.this,MainActivity.class);
+                Toast.makeText(getApplicationContext(),"Logged Out!", Toast.LENGTH_SHORT).show();
+                startActivity(i);
+                overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
+            }
+        });
     }
 
     @Override
@@ -78,32 +97,172 @@ public class ProfileActivity extends AppCompatActivity {
         });
     }
 
-    public void ChangePassword(EditText oldPass) {
-/*
-        String oldPass_str = oldPass.getText().toString();
-        String oldDB="aaaa";
+    public void ChangePassword() {
+        Button buttonChange = findViewById(R.id.change_but);
+
         buttonChange.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (oldDB.equals(oldPass_str)) {
-                    Context context1 = getApplicationContext();
-                    Toast toast1 = Toast.makeText(getApplicationContext(), "Password Updated!", Toast.LENGTH_SHORT);
-                    toast1.show();
-                }
-                else{
-                    Context context2 = getApplicationContext();
-                    Toast toast2 = Toast.makeText(getApplicationContext(), "ERROR", Toast.LENGTH_SHORT);
-                    toast2.show();
-                }
-            }
-        });*/
-        buttonChange.setOnClickListener(event -> {
-            if (oldPass.getText().equals("xxxx")) {
-                Toast.makeText(getApplicationContext(), "Password updated!", Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(getApplicationContext(), "Error!", Toast.LENGTH_SHORT).show();
+                ProfileActivity.getUser getPass = new ProfileActivity.getUser();
+                getPass.execute(current_email);
             }
         });
+
+
+    }
+
+    private class getUser extends AsyncTask<String, Void, String> {
+        String res = "";
+        Connection con = null;
+
+        @Override
+        protected String doInBackground(String... params) {
+            String def_password = "";
+            try {
+                ResultSet rs = null;
+                Class.forName("com.mysql.jdbc.Driver");
+                Connection con = DriverManager.getConnection(url, user, pass);
+                Statement st = con.createStatement();
+
+                String query = "SELECT password FROM user WHERE email=\""+params[0]+"\"";
+                rs = st.executeQuery(query);
+
+                if(rs.next()){
+                    def_password = rs.getString("password");
+                }
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+                res = e.toString();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } finally {
+                if(con != null){
+                    try {
+                        con.close();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return def_password;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            EditText oldPass, newPass;
+            newPass = (EditText) findViewById(R.id.editNewPassword);
+            oldPass = (EditText) findViewById(R.id.editOldPassword);
+
+            String Old, New;
+            New = String.valueOf(newPass.getText());
+            Old = String.valueOf(oldPass.getText());
+
+            if(Old.equals(result)){
+                ProfileActivity.setNewPassword setPassWord = new ProfileActivity.setNewPassword();
+                setPassWord.execute(New);
+            }else{
+                Toast.makeText(getApplicationContext(),"Incorrect Old Password!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private class setNewPassword extends AsyncTask<String, Void, Integer> {
+        String res = "";
+        Connection con = null;
+
+        @Override
+        protected Integer doInBackground(String... params) {
+            int worked = 0;
+            try {
+                ResultSet rs = null;
+                Class.forName("com.mysql.jdbc.Driver");
+                Connection con = DriverManager.getConnection(url, user, pass);
+                Statement st = con.createStatement();
+
+                String query = "UPDATE user SET password=\""+params[0]+"\" WHERE email = \""+current_email+"\"";
+                worked = st.executeUpdate(query);
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+                res = e.toString();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } finally {
+                if(con != null){
+                    try {
+                        con.close();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return worked;
+        }
+
+        @Override
+        protected void onPostExecute(Integer result) {
+            EditText oldPass, newPass;
+
+            if(result != 0){
+                newPass = (EditText) findViewById(R.id.editNewPassword);
+                oldPass = (EditText) findViewById(R.id.editOldPassword);
+                newPass.setText("");
+                oldPass.setText("");
+
+                Toast.makeText(getApplicationContext(),"Password Changed Successfully!", Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(getApplicationContext(),"Error Changing Password!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private class displayNameEmail extends AsyncTask<String, Void, String> {
+        String res = "";
+        Connection con = null;
+
+        @Override
+        protected String doInBackground(String... params) {
+            String name = "";
+            try {
+                ResultSet rs = null;
+                Class.forName("com.mysql.jdbc.Driver");
+                Connection con = DriverManager.getConnection(url, user, pass);
+                Statement st = con.createStatement();
+
+                String query = "SELECT name FROM user WHERE email=\""+current_email+"\"";
+                rs = st.executeQuery(query);
+
+                if(rs.next()){
+                    name = rs.getString("name");
+                }
+
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+                res = e.toString();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } finally {
+                if(con != null){
+                    try {
+                        con.close();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return name;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            TextView nameTv = (TextView) findViewById(R.id.name_mypage);
+            TextView emailTv = (TextView) findViewById(R.id.email_mypage);
+
+            nameTv.setText(result);
+            emailTv.setText(current_email);
+        }
     }
 }
 
