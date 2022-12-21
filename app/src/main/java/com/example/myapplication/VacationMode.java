4d06b5e4414aa3a35da781e6c6ea7148d4f04e07
claudia-ribeiro <59340144+claudia-ribeiro@users.noreply.github.com>
2022-12-21 12:01:23 +0000

    pop up 2 atualizado

--- app/src/main/java/com/example/myapplication/VacationMode.java
+++ app/src/main/java/com/example/myapplication/VacationMode.java
@@ -2,22 +2,44 @@ package com.example.myapplication;
 
 import android.app.AlertDialog;
 import android.app.TimePickerDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.SeekBar;
+import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.TimePicker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.myapplication.models.smart;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Locale;
 
 public class VacationMode extends AppCompatActivity {
-    Button startTime, endTime;
-    int hour1, minute1, hour2, minute2;
+    private static final String url = "jdbc:mysql://34.163.74.192/mydb?autoReconnect=true&useSSL=false";
+    private static final String user = "root";
+    private static final String pass = "db-es-teamf";
 
     String current_email="";
     String current_house_id="";
+    String selected_device_id="";
+    String selected_device_name="";
+
+    TextView tV;
+    SeekBar seekBar;
+
+    Button startTime, endTime;
+    int hour1, minute1, hour2, minute2, second1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +67,9 @@ public class VacationMode extends AppCompatActivity {
             }
         });
 
+        VacationMode.retrieveValuesFromDB2 retrieveValuesFromDB2 = new VacationMode.retrieveValuesFromDB2();
+        retrieveValuesFromDB2.execute();
+
     }
 
     public void closeWindow(){
@@ -58,7 +83,22 @@ public class VacationMode extends AppCompatActivity {
             public void onTimeSet(TimePicker timePicker1, int selectedHour1, int selectedMinute1) {
                 hour1 = selectedHour1;
                 minute1 = selectedMinute1;
+                second1 = 0;
                 startTime.setText(String.format(Locale.getDefault(), "%02d:%02d", hour1, minute1));
+                String hour_1 = Integer.toString(hour1);
+                String minute_1 = Integer.toString(minute1);
+                int startingTime;
+                if (minute1 < 10) {
+                    startingTime = Integer.valueOf(String.valueOf(hour1) + String.valueOf(second1) + String.valueOf(minute1) + String.valueOf(second1) + String.valueOf(second1));
+                }
+                else {
+                    startingTime = Integer.valueOf(String.valueOf(hour1) + String.valueOf(minute1) + String.valueOf(second1) + String.valueOf(second1));
+                }
+                String startingTime2 = Integer.toString(startingTime);
+                System.out.println("sao" + startingTime2 + "horas");
+                VacationMode.updateAction2ParameterOnDB updateAction2ParameterOnDB = new VacationMode.updateAction2ParameterOnDB();
+                updateAction2ParameterOnDB.execute("2", "start_time", startingTime2);
+
             }
         };
 
@@ -76,6 +116,20 @@ public class VacationMode extends AppCompatActivity {
                 hour2 = selectedHour2;
                 minute2 = selectedMinute2;
                 endTime.setText(String.format(Locale.getDefault(), "%02d:%02d", hour2, minute2));
+                String hour_2 = Integer.toString(hour2);
+                String minute_2 = Integer.toString(minute2);
+                int startingTime;
+                if (minute2 < 10) {
+                    startingTime = Integer.valueOf(String.valueOf(hour2) + String.valueOf(second1) + String.valueOf(minute2) + String.valueOf(second1) + String.valueOf(second1));
+                }
+                else {
+                    startingTime = Integer.valueOf(String.valueOf(hour2) + String.valueOf(minute2) + String.valueOf(second1) + String.valueOf(second1));
+                }
+                String startingTime2 = Integer.toString(startingTime);
+                System.out.println("sao" + startingTime2 + "horas");
+                VacationMode.updateAction2ParameterOnDB updateAction2ParameterOnDB = new VacationMode.updateAction2ParameterOnDB();
+                updateAction2ParameterOnDB.execute("2", "end_time", startingTime2);
+
             }
         };
 
@@ -85,5 +139,190 @@ public class VacationMode extends AppCompatActivity {
         timePickerDialog.setTitle("Select End Time");
         timePickerDialog.show();
     }
+
+    private class updateAction2ParameterOnDB extends AsyncTask<String, Void, String> {
+        // params[0] = room id (string)
+        // params[1] = parameter manual/brightness/motion (string)
+        // params[2] = value
+        String res = "";
+        Connection con = null;
+
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                ResultSet rs = null;
+                Class.forName("com.mysql.jdbc.Driver");
+                Connection con = DriverManager.getConnection(url, user, pass);
+                Statement st = con.createStatement();
+                String id = params[0];
+                String parameter = params[1];
+                int value = Integer.parseInt(params[2]);
+
+                String query =  "UPDATE timed_action " +
+                        "SET "+parameter+"= " + value + " " +
+                        "WHERE timed_action.id=\""+id+"\"";
+
+                st.executeUpdate(query);
+
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+                res = e.toString();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } finally {
+                if(con != null){
+                    try {
+                        con.close();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return "";
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+        }
+    }
+
+
+    private class retrieveValuesFromDB2 extends AsyncTask<String, Void, smart> {
+        String res = "";
+        Connection con = null;
+        smart smart1;
+        @Override
+        protected smart doInBackground(String... params) {
+            try {
+                ResultSet rs = null;
+                Class.forName("com.mysql.jdbc.Driver");
+                Connection con = DriverManager.getConnection(url, user, pass);
+                Statement st = con.createStatement();
+
+                String query = "SELECT " +
+                        "timed_action.room_id AS division, " +
+                        "timed_action.id AS id, " +
+                        "timed_action.scheduled AS smart_on, " +
+                        "timed_action.brightness AS brightness, " +
+                        "timed_action.motion_sense AS motion_sense, " +
+                        "timed_action.start_time AS start_time, " +
+                        "timed_action.end_time AS end_time " +
+                        "FROM room " +
+                        "JOIN timed_action " +
+                        "ON room.id = timed_action.room_id " +
+                        "WHERE " +
+                        "room.house_id =" + current_house_id;
+
+                rs = st.executeQuery(query);
+
+                rs.next();
+                rs.next();
+                //while (!Objects.equals(rs.getString("id"), "1")) {
+                //    rs.next();
+                //}
+
+                String id = rs.getString("id");
+                String division = rs.getString("division");
+                int brightness = rs.getInt("brightness");
+                int smart_on = rs.getInt("smart_on");
+                int motion_sense = rs.getInt("motion_sense");
+                int start_time = rs.getInt("start_time");
+                int end_time = rs.getInt("end_time");
+                smart1 = new smart(id, division, smart_on, brightness, motion_sense, start_time, end_time);
+
+
+
+                System.out.println("CHEGUEI AQUI");
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+                res = e.toString();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } finally {
+                if (con != null) {
+                    try {
+                        con.close();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            System.out.println(smart1);
+            return smart1;
+        }
+
+        @Override
+        protected void onPostExecute(smart result) {
+            int curr_motion_sense, curr_brightness, curr_smart_on, curr_start_time;
+
+            //Setup ao icone da brightness
+            //Setup à SeekBar de controlo da Brightness
+            result.smart_setsB((SeekBar) findViewById(R.id.light2) );
+            curr_brightness = result.smart_getBrightness();
+            result.smart_getsB().setProgress(curr_brightness);
+            result.smart_getsB().setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser){
+                    result.smart_setBrightness( progress );
+                }
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {}
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar){
+                    String id = result.smart_getId();
+                    System.out.println(id);
+                    String brightness = String.valueOf(result.smart_getBrightness());
+                    VacationMode.updateAction2ParameterOnDB updateAction2ParameterOnDB = new VacationMode.updateAction2ParameterOnDB();
+                    updateAction2ParameterOnDB.execute(id, "brightness", brightness);
+                }
+            });
+
+
+            Switch motion_smart1 = (Switch) findViewById(R.id.motion_smart2);
+            result.smart_setMotion_switch(motion_smart1);
+            curr_motion_sense = result.smart_getMotion_sense();
+            result.smart_getMotion_switch().setChecked(curr_motion_sense == 1);
+            result.smart_getMotion_switch().setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    String id = result.smart_getId();
+                    if (isChecked) {
+                        result.smart_setMotion_sense(1);
+                        VacationMode.updateAction2ParameterOnDB updateAction2ParameterOnDB = new VacationMode.updateAction2ParameterOnDB();
+                        updateAction2ParameterOnDB.execute(id, "motion_sense", "1");
+                    } else {
+                        result.smart_setMotion_sense(1);
+                        VacationMode.updateAction2ParameterOnDB updateAction2ParameterOnDB = new VacationMode.updateAction2ParameterOnDB();
+                        updateAction2ParameterOnDB.execute(id, "motion_sense", "0");
+                    }
+                }
+            });
+
+            result.smart_setON_switch((Switch) findViewById(R.id.action_2));
+            curr_smart_on = result.smart_getOn();
+            result.smart_getON_Switch().setChecked(curr_smart_on==1);
+            result.smart_getON_Switch().setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    String id = result.smart_getId();
+                    if(isChecked){
+                        result.smart_setOn(1);
+                        VacationMode.updateAction2ParameterOnDB updateAction2ParameterOnDB = new VacationMode.updateAction2ParameterOnDB();
+                        updateAction2ParameterOnDB.execute(id, "scheduled", "1");
+                    }
+                    else{
+                        result.smart_setOn(0);
+                        VacationMode.updateAction2ParameterOnDB updateAction2ParameterOnDB = new VacationMode.updateAction2ParameterOnDB();
+                        updateAction2ParameterOnDB.execute(id, "scheduled", "0");
+                    }
+                }
+            });
+
+
+        }
+    }
+
 
 }
